6e919e74422b639e2fb1f883c68b7fd94879c1d6
Your Vika <pasviktoria2@gmail.com>
2024-06-22 10:34:46 +0200

    int proyecto Ecom

---
+++ controlers/database.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('goshop', 'root', 'vadim20***14', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
