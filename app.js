d458e727f77547907105e9d06310ed59dd498b55
Your Vika <pasviktoria2@gmail.com>
2024-06-24 19:27:56 +0200

    nuevo cambio

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const sequelize = require('./controlers/database'); // Asegúrate de que este es el archivo correcto
 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Prueba la conexión a la base de datos
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Define una ruta simple para verificar que el servidor está funcionando
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
